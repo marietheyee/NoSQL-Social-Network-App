483ead823a18758564bab81fb14b18f42a3b2893
Marie Yee <marieyee02@yahoo.com>
2022-07-25 19:34:26 -0700

    Come on...

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-
+//dependencies
 const express = require('express');
 const mongoose = require('mongoose');
+
+// routing
 const routes = require('./routes');
 
+// express & port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-
+// connect db to mongoose
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/27017',
   {
@@ -19,6 +22,7 @@ mongoose.connect(
     useUnifiedTopology: true,
 });
 
+// Use this to log mongo queries being executed
 mongoose.set('debug', true);
 
 app.use(require('./routes'));
